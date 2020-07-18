d1f2f8688194e9b869bad607328f6fc38efe2b96
Fatih-Sevim <fatihsevimtr@gmail.com>
2020-07-18 14:28:50 +0200

    Starting out with expressions

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,12 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello, Fatih");
         int firstNumber=5+10*12*2-(4-2);
+        int secondNumber=15;
+        int thirdNumber=firstNumber+3;
+        int total=firstNumber+secondNumber+thirdNumber;
+        int lastNumber=2000-total;
         System.out.println(firstNumber);
+        System.out.println(total);
+        System.out.println(lastNumber);
     }
 }
