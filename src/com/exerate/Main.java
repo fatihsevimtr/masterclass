7f5da26bb6dc6bb2ff2a68fbaa02f6c758481ad9
Fatih-Sevim <fatihsevimtr@gmail.com>
2020-07-18 14:49:48 +0200

    1. Primitive Types, 2. Wrapper Classes, 3. Integer Value Range, 4. Overflow and underflow 5. Underscore notation for easy reading the numbers

---
+++ src/com/exerate/Main.java
@@ -0,0 +1,19 @@
+package com.exerate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myValue=1000;
+        int maxIntValue=Integer.MAX_VALUE;
+        int minIntValue=Integer.MIN_VALUE;
+        System.out.println("The max int value is "+maxIntValue);
+        System.out.println("The min int value is "+minIntValue);
+        System.out.println("Over flowed max int value:"+(maxIntValue+1));
+        System.out.println("Under flowed min int value:"+(minIntValue-1));
+        int segmentedInt=2_147_483_647;
+        int testNum=3_5487_547;
+        //Wrapper classes for primitive types: Byte,Short,Integer,Long,Float, Double, Character, Boolean
+
+
+    }
+}
