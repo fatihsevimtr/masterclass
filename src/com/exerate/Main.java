429c3b7c064586ac8d06387d7c78c8c98fea25a1
Fatih-Sevim <fatihsevimtr@gmail.com>
2020-07-18 16:21:42 +0200

    Type Casting, Java Default Value: integer, L notation

--- src/com/exerate/Main.java
+++ src/com/exerate/Main.java
@@ -15,5 +15,33 @@ public class Main {
         //Wrapper classes for primitive types: Byte,Short,Integer,Long,Float, Double, Character, Boolean
 
 
+        byte maxByteValue=Byte.MAX_VALUE;
+        byte minByteValue=Byte.MIN_VALUE;
+        System.out.println("The max byte value is "+maxByteValue);
+        System.out.println("The min byte value is "+minByteValue);
+
+        short maxShortValue=Short.MAX_VALUE;
+        short minShortValue=Short.MIN_VALUE;
+        System.out.println("The max short value is "+maxShortValue);
+        System.out.println("The min short value is "+minShortValue);
+
+        long maxLongValue=Long.MAX_VALUE;
+        long minLongValue=Long.MIN_VALUE;
+        System.out.println("The max long value is "+maxLongValue);
+        System.out.println("The min long value is "+minLongValue);
+
+        //long testLon=2147483648; error due to java treating as integer by default number
+        long testLon=2147483648L; // and type casting if needed as follow below.
+        System.out.println(testLon);
+        int newInt=maxIntValue/2;
+        byte newByte= (byte) (maxByteValue/2); //it requires typ casting even though it fits.
+        short newShort= (short) (maxShortValue/2);
+
+        byte x=60;
+        byte y=20;
+        byte z=(byte)(x+y);
+        System.out.println(z);
+        System.out.println(x+y);//if no assignment no error...
+
     }
 }
