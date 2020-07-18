c2aca399d16acc6dbacacec29832b285ae29b6a6
Fatih-Sevim <fatihsevimtr@gmail.com>
2020-07-18 16:51:13 +0200

    First Commit

---
+++ src/com/exerate/Main.java
@@ -0,0 +1,23 @@
+package com.exerate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float minFloat=Float.MIN_VALUE;
+        float maxFloat=Float.MAX_VALUE;
+        System.out.println("Min float value="+minFloat);
+        System.out.println("Max float value="+maxFloat);
+
+
+        double minDouble=Double.MIN_VALUE;
+        double maxDouble=Double.MAX_VALUE;
+        System.out.println("Min Double value="+minDouble);
+        System.out.println("Max Double value="+maxDouble);
+
+        int intValue=5;
+        float floatValue=5.45f;
+        double doubleValue=5.45d;
+
+    }
+}
