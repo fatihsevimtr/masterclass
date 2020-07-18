7797548088e705e288fa0df810c9eeedb1449df2
Fatih-Sevim <fatihsevimtr@gmail.com>
2020-07-18 13:26:53 +0200

    First variable with some operations exercise.

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello, Fatih");
+        int firstNumber=5+10*12*2-(4-2);
+        System.out.println(firstNumber);
     }
 }
