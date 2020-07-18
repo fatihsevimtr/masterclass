385f1f74c6f8eed470c42c7bdd7a1431a973d6a7
Fatih-Sevim <fatihsevimtr@gmail.com>
2020-07-18 18:26:16 +0200

    Char and boolean, unicode-table.com

---
+++ src/com/exerate/Main.java
@@ -0,0 +1,23 @@
+package com.exerate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar='D';
+        char myCharUnicode='\u0044';
+
+        System.out.println("Letter "+myChar);
+        System.out.println("Letter "+myCharUnicode);
+        char copyRight='\u00A9';
+
+
+        System.out.println(copyRight);
+
+        boolean valueTrue=true;
+        boolean valueFalse=false;
+        boolean isClientAgeOver18=true;
+        System.out.println(isClientAgeOver18);
+
+    }
+}
